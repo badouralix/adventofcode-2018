25a637b34acfbf2cc15fa49a965a17b276e000be
Jonathan Remy <jonremy@users.noreply.github.com>
2018-12-01 00:50:10 -0500

    [jon] d1p2

---
+++ day-01/part-2/jon.py
@@ -0,0 +1,14 @@
+from runners.python import SubmissionPy
+import collections
+
+class JonSubmission(SubmissionPy):
+
+    def run(self, s):
+        l = [int(x) for x in s.split("\n")]
+        d = {0}
+        s = 0
+        for i in range(100000000): # eh oui
+            s += l[i%len(l)]
+            if s in d:
+                return s
+            d.add(s)
