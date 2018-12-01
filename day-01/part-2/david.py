9517e294d2741729a194feeefcf872195ea46288
David Dos Santos <gdavid.dos@gmail.com>
2018-12-01 00:51:44 -0500

    [david] oops

--- day-01/part-2/david.py
+++ day-01/part-2/david.py
@@ -5,8 +5,10 @@ class DavidSubmission(SubmissionPy):
     def run(self, s):
         seen = set()
         x = 0
-        while x not in seen:
+        while True:
             for line in s.split("\n"):
                 n = int(line[1:])
                 x += (n if line[0] == "+" else -n)
-        return x
+                if x in seen:
+                    return x
+                seen.add(x)
