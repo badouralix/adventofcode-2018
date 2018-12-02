17fddae389aca0c86e1ee35f8a7b98b797a76c39
Ludovico <ludoalta@gmail.com>
2018-12-02 13:26:48 +0100

    ludoge day 2

---
+++ day-02/part-2/ludoge.py
@@ -0,0 +1,26 @@
+from runners.python import SubmissionPy
+
+
+class LudogeSubmission(SubmissionPy):
+
+    def run(self, s):
+        # :param s: input in string format
+        # :return: solution flag
+        # Your code goes here
+        s_list = s.splitlines()
+        for i in range(len(s_list)):
+            for j in range(i+1, len(s_list)):
+                m = self.match(s_list[i], s_list[j])
+                if m is not None:
+                    return m
+        pass
+
+    def match(self, a, b):
+        k = -1
+        for i in range(len(a)):
+            if a[i] != b[i]:
+                if k == -1:
+                    k = i
+                else:
+                    return None
+        return a[:k]+a[k+1:]
