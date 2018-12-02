7030d554ab419841621e1d432be8eaae679411ef
Rémi Calixte <remi.calixte@datadoghq.com>
2018-12-02 10:41:57 -0500

    day2part2

---
+++ day-02/part-2/remicalixte.rs
@@ -0,0 +1,53 @@
+use std::env::args;
+
+fn main() {
+    println!("{}", run(args().nth(1).expect("Please provide an input")))
+}
+
+fn run(input: String) -> String {
+    // Your code goes here
+    // println!("{}", input);
+    let lines: Vec<&str> = input.lines().collect();
+    // println!("{}", lines.len());
+    let mut res =   String::new();
+    for i in 0..lines.len() {
+        for j in 0..(i+1) {
+            // println!("lines {}", lines[i]);
+            // println!("lines {}", lines[j]);
+            let mut diff = 0;
+            let mut diff_index = 0;
+            let mut iter = lines[i].bytes().zip(lines[j].bytes()).enumerate();
+            loop {
+                match iter.next() {
+                    Some((k, (b1, b2))) => {
+                        if  b1 != b2 {
+                            diff += 1;
+                            diff_index = k;    
+                        }
+                        if diff > 1 {
+                            break;
+                        }
+                    },
+                    None => break
+                }
+            }
+            if diff == 1 {
+                res = lines[i].to_string();
+                res.remove(diff_index);
+            }
+
+
+        }
+    }
+    res
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn run_test() {
+        assert_eq!(run("abc\nadc\n".to_string()), "ac".to_string())
+    }
+}
