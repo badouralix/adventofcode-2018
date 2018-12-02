acc23d3657565c8eb9a2a71e3a61b5347de06412
Jonathan Remy <jonremy@users.noreply.github.com>
2018-12-02 00:42:22 -0500

    [jon] d2p2 cleaner

--- day-02/part-2/jon.py
+++ day-02/part-2/jon.py
@@ -10,8 +10,10 @@ class JonSubmission(SubmissionPy):
         for i in range(len(l)):
             for j in range(i+1, len(l)):
                 if hamming(l[i], l[j]) == 1:
-                    return "".join(l[i][k] for k in range(len(l[i])) if l[i][k] == l[j][k])
-
+                    return comstr(l[i], l[j])
 
 def hamming(a, b):
-    return sum(1 for i in range(len(a)) if a[i] != b[i])
+    return sum(1 for k in range(len(a)) if a[k] != b[k])
+
+def comstr(a, b):
+    return "".join(a[k] for k in range(len(a)) if a[k] == b[k])
