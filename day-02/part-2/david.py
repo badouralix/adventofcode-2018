6f932e883af31a303e615995d8218c21b133723f
David Dos Santos <gdavid.dos@gmail.com>
2018-12-02 02:06:39 -0500

    [david] cleanup day 2

--- day-02/part-2/david.py
+++ day-02/part-2/david.py
@@ -1,17 +1,17 @@
 from runners.python import SubmissionPy
 
+from itertools import combinations
+
 class DavidSubmission(SubmissionPy):
 
     def distance(self, w1, w2):
         # returns the distance between two words
         # it is the number of letters for which the two words differ
-        n = len(w1)
-        return sum(1 if w1[i] != w2[i] else 0 for i in range(n))
+        return sum(1 if w1[i] != w2[i] else 0 for i in range(len(w1)))
 
     def run(self, s):
         words = s.split("\n")
         n = len(words[0])
-        for w1 in words:
-            for w2 in words:
-                if self.distance(w1, w2) == 1:
-                    return "".join(w1[i] for i in range(n) if w1[i]==w2[i])
+        for w1, w2 in combinations(words, 2):
+            if self.distance(w1, w2) == 1:
+                return "".join(w1[i] for i in range(n) if w1[i]==w2[i])
