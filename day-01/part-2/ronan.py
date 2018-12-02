12d292b2a706b71b75e2a45d97a33b241b693961
Ronan Pelliard <ronan.pelliard@gmail.com>
2018-12-02 10:36:56 -0500

    [day 1] add part 2

---
+++ day-01/part-2/ronan.py
@@ -0,0 +1,14 @@
+
+from runners.python import SubmissionPy
+class RonanSubmission(SubmissionPy):
+    @staticmethod
+    def run(input):
+        sum = 0
+        seen = set([sum])
+        freqs = [int(x) for x in input.split("\n")]
+        while 1:
+            for n in freqs:
+                sum += int(n)
+                if sum in seen:
+                    return sum
+                seen.add(sum)
