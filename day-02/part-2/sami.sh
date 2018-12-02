b044609ccfb11a6e9551da0bf07a9047e2dc5bd4
Sami Tabet <sami.tabet@datadoghq.com>
2018-12-02 13:54:01 -0500

    fix

--- day-02/part-2/sami.sh
+++ day-02/part-2/sami.sh
@@ -3,7 +3,7 @@
 input=($(echo $1 | tr ' ' '\n'))
 length=${#input[0]}
 
-for i in $(seq 1 $((length - 2))); do
+for i in $(seq 1 $((length - 1))); do
     res=$(echo "${input[@]}" | tr ' ' '\n' | cut -c -$i,$(($i+2))- | sort | uniq -c | grep 2)
     if [[ -n $res ]]; then
         echo $res | cut -f2 -d' '
