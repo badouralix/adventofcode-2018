ae2a56ea564679dbd3ed9e7154895bb6944c118d
Ayc0 <ayc0.benj@gmail.com>
2018-12-02 14:06:15 +0100

    Ayc0 day 2

---
+++ day-02/part-1/ayc0.js
@@ -0,0 +1,40 @@
+/**
+ * @param {{string}} s puzzle input in string format
+ * @returns solution flag
+ */
+const run = s => {
+  // Your code goes here
+  const [twos, threes] = s
+    .split("\n")
+    .map(line =>
+      line.split("").reduce((letters, letter) => {
+        letters[letter] = 1 + (letters[letter] || 0);
+        return letters;
+      }, {})
+    )
+    .reduce(
+      (acc, line) => {
+        let findTwo = false;
+        let findThree = false;
+        Object.values(line).forEach(count => {
+          if (count === 2) {
+            findTwo = true;
+          }
+          if (count === 3) {
+            findThree = true;
+          }
+        });
+        if (findThree) {
+          acc[1] += 1;
+        }
+        if (findTwo) {
+          acc[0] += 1;
+        }
+        return acc;
+      },
+      [0, 0]
+    );
+  return twos * threes;
+};
+
+console.log(run(process.argv[2]));
