46dd4951a8099151f8119aa37c88e15f48c54651
lithium <louis.cabrol@student.ecp.fr>
2018-12-02 11:29:47 +0100

    smol gains

--- day-01/part-2/lithium.cpp
+++ day-01/part-2/lithium.cpp
@@ -42,11 +42,12 @@ string run(string s)
 
 int main(int argc, char **argv)
 {
-    if (argc < 2)
-    {
-        cout << "Missing one argument" << endl;
-        exit(1);
-    }
+    // gain some µs
+    // if (argc < 2)
+    // {
+    //     cout << "Missing one argument" << endl;
+    //     exit(1);
+    // }
     cout << run(string(argv[1])) << "\n";
     return 0;
 }
