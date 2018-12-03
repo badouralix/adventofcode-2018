c902e170c71e3b98644eaa97f59b622a183a75f0
Silvestre Perret <silvestre.perret@gmail.com>
2018-12-03 18:31:48 +0100

    silvestre/day-1

--- day-01/part-2/silvestre.py
+++ day-01/part-2/silvestre.py
@@ -8,13 +8,12 @@ class SilvestreSubmission(SubmissionPy):
         # :return: solution flag
         array = np.fromstring(s, dtype=np.int64, sep="\n")
         reached = set()
-        curr = 0
-        i = 0
-        while True:
-            if curr in reached or i > 150000: # Security
-                return curr
-            else:
-                reached.add(curr)
-                curr += array[i%array.size]
-                i += 1
-        return 0
+        curr = loop = 0
+        while loop < 1000:
+            for el in array:
+                if curr in reached:
+                    return curr
+                else:
+                    reached.add(curr)
+                    curr += el
+        print('Error: Never reached the same frequency twice')
