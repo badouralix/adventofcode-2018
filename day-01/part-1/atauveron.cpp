c0f99f5de86563018be82d29a8ac717305de7c55
Aimery Tauveron <atauveron@via.ecp.fr>
2018-12-03 09:19:24 +0100

    [atauveron] first attempt at day 1

---
+++ day-01/part-1/atauveron.cpp
@@ -0,0 +1,29 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+
+int process(std::string filename) {
+  int frq (0);
+  int line;
+  std::ifstream input;
+  input.open(filename);
+  if (input.is_open()){
+    while (input >> line){
+      frq += line;
+    }
+  }
+  return frq;
+}
+
+int main(int argc, char **argv) {
+  /*
+   * Usage: <executable> <input file>
+   */
+  if (argc<2){
+    std::cout << "No file was provided." << std::endl;
+    return 0;
+  }
+  std::string filename (argv(1));
+  std::cout << "The frequency is: " << process(filename) << std::endl;
+  return 0;
+}
