ba3e0ac660d544b03b987a2d3544d60bcb2a53b4
Aimery Tauveron <atauveron@via.ecp.fr>
2018-12-03 15:00:43 +0100

    Revert previous commit: c++14 is not supported

--- runners/cpp.py
+++ runners/cpp.py
@@ -19,7 +19,7 @@ class SubmissionCpp(SubmissionWrapper):
         SubmissionWrapper.__init__(self)
         tmp = tempfile.NamedTemporaryFile(prefix="aoc")
         tmp.close()
-        compile_output = subprocess.check_output(["g++", "-Wall", "-O3", "-std=c++14", "-o", tmp.name, file]).decode()
+        compile_output = subprocess.check_output(["g++", "-Wall", "-O3", "-std=c++11", "-o", tmp.name, file]).decode()
         if compile_output:
             raise CompilationError(compile_output)
         self.executable = tmp.name
