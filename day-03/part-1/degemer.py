607d147c60d773e9530f6a7491d36c59b675fd3e
Quentin Madec <quentin.madec@datadoghq.com>
2018-12-03 00:20:41 -0500

    degemer/day-3

---
+++ day-03/part-1/degemer.py
@@ -0,0 +1,25 @@
+from runners.python import SubmissionPy
+import numpy as np
+import ipdb as pdb
+
+
+class DegemerSubmission(SubmissionPy):
+
+    def run(self, s):
+        # :param s: input in string format
+        # :return: solution flag
+        # Your code goes here
+        claim = np.empty((1000, 1000), dtype=np.int)
+        claim[:] = 0
+
+        for case in s.splitlines():
+            _, t = case.split("@")
+            tops, sizes = t.split(':')
+            left, top = map(lambda x: int(x.strip()), tops.split(','))
+            width, height = map(lambda x: int(x.strip()), sizes.split('x'))
+            #pdb.set_trace()
+            claim[left:left + width, top:top + height] += 1
+
+        claim[claim <= 1] = 0
+        claim[claim >= 2] = 1
+        return np.sum(claim)
---
+++ day-03/part-2/degemer.py
@@ -0,0 +1,29 @@
+from runners.python import SubmissionPy
+import numpy as np
+
+
+class DegemerSubmission(SubmissionPy):
+
+    def run(self, s):
+        # :param s: input in string format
+        # :return: solution flag
+        # Your code goes here
+        claim = np.empty((1000, 1000), dtype=np.int)
+        claim[:] = 0
+
+        for case in s.splitlines():
+            _, t = case.split("@")
+            tops, sizes = t.split(':')
+            left, top = map(lambda x: int(x.strip()), tops.split(','))
+            width, height = map(lambda x: int(x.strip()), sizes.split('x'))
+            claim[left:left + width, top:top + height] += 1
+
+        for case  in s.splitlines():
+            id, t = case.split("@")
+            _, id = id.split('#')
+            tops, sizes = t.split(':')
+            left, top = map(lambda x: int(x.strip()), tops.split(','))
+            width, height = map(lambda x: int(x.strip()), sizes.split('x'))
+            if np.all(claim[left:left + width, top:top + height] == 1):
+                return id
+
