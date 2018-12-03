633c5998835875fc9a3b31f1dc39bec466d9dc8b
JulesDT <jules.denardou@datadoghq.com>
2018-12-03 18:35:45 -0500

    day3 in cpp

---
+++ day-03/part-2/jules.cpp
@@ -0,0 +1,59 @@
+#include <string>
+#include <sstream>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string run(string s) {
+    string   line;
+    int map[1000][1000] = {{0}};
+    stringstream ss(s);
+    while(getline(ss, line, '\n')){
+        size_t at = line.find("@");
+        size_t comma = line.find(",");
+        size_t semi_colon = line.find(":");
+        size_t multiplier = line.find("x");
+        int x = stoi(line.substr(at + 1, comma - at - 1));
+        int y = stoi(line.substr(comma + 1, semi_colon - comma - 1));
+        int width = stoi(line.substr(semi_colon + 1, multiplier - semi_colon - 1));
+        int height = stoi(line.substr(multiplier + 1, line.length() - multiplier - 1));
+        for(int i = x; i < x + width; i++){
+            for(int j = y; j < y + height; j++){
+                ++map[i][j];
+            }
+        }
+    }
+    stringstream ss2(s);
+    while(getline(ss2, line, '\n')){
+        size_t at = line.find("@");
+        size_t comma = line.find(",");
+        size_t semi_colon = line.find(":");
+        size_t multiplier = line.find("x");
+        int x = stoi(line.substr(at + 1, comma - at - 1));
+        int y = stoi(line.substr(comma + 1, semi_colon - comma - 1));
+        int width = stoi(line.substr(semi_colon + 1, multiplier - semi_colon - 1));
+        int height = stoi(line.substr(multiplier + 1, line.length() - multiplier - 1));
+        bool is_one_non_1 = false;
+        for(int i = x; i < x + width && !is_one_non_1; i++){
+            for(int j = y; j < y + height && !is_one_non_1; j++){
+                if(map[i][j] >= 2){
+                    is_one_non_1 = true;
+                }
+            }
+        }
+        if(!is_one_non_1){
+            return line.substr(1, at - 2);
+        }
+    }
+    return "";
+}
+
+int main(int argc, char** argv) {
+    if (argc < 2) {
+        cout << "Missing one argument" << endl;
+        exit(1);
+    }
+    cout << run(string(argv[1])) << "\n";
+    return 0;
+}
