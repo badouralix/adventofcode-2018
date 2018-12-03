73480a95493fe72d5c9c0c46478f972a31b4ebfc
Aimery Tauveron <atauveron@via.ecp.fr>
2018-12-03 11:22:51 +0100

    [atauveron] day-01 part-1

---
+++ day-02/part-1/atauveron.cpp
@@ -0,0 +1,47 @@
+#include <array>
+#include <fstream>
+#include <iostream>
+#include <string>
+
+int process(std::string filename) {
+  unsigned int two(0);
+  unsigned int three(0);
+  std::string line;
+  std::ifstream input;
+  input.open(filename);
+  if (input.is_open()) {
+    while (input >> line) {
+      // Array of frequencies (initialized)
+      std::array<uint_fast8_t, 26> occurences{};
+      for (char &c : line) {
+        uint_fast8_t index = (uint_fast8_t)c - 97; // Offset in ASCII table
+        occurences[index] += 1;
+      }
+      bool flag_two(true);
+      bool flag_three(true);
+      for (auto &it : occurences) {
+        if (flag_two && it == 2) {
+          two += 1;
+          flag_two = false;
+        } else if (flag_three && it == 3) {
+          three += 1;
+          flag_three = false;
+        }
+      }
+    }
+  }
+  return two * three;
+}
+
+int main(int argc, char **argv) {
+  /*
+   * Usage: <executable> <input file>
+   */
+  if (argc < 2) {
+    std::cout << "No file was provided." << std::endl;
+    return 0;
+  }
+  std::string filename(argv[1]);
+  std::cout << process(filename) << std::endl;
+  return 0;
+}
