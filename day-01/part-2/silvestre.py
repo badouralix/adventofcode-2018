1f08b3fee1f64a739894a149b54c2a9e3aaec3cf
Silvestre Perret <silvestre.perret@gmail.com>
2018-12-03 18:07:03 +0100

    silvestre/day-1

---
+++ day-01/part-2/silvestre.py
@@ -0,0 +1,20 @@
+from runners.python import SubmissionPy
+import numpy as np
+
+class SilvestreSubmission(SubmissionPy):
+
+    def run(self, s):
+        # :param s: input in string format
+        # :return: solution flag
+        array = np.fromstring(s, dtype=np.int64, sep="\n")
+        reached = set()
+        curr = 0
+        i = 0
+        while True:
+            if curr in reached or i > 150000:
+                return curr
+            else:
+                reached.add(curr)
+                curr += array[i%array.size]
+                i += 1
+        return 0
