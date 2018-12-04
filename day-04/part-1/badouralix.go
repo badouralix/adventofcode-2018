9e6955de3bd548616a111c9ec8d4704782dd9034
Ayaz Badouraly <ayaz.badouraly@via.ecp.fr>
2018-12-04 01:47:29 -0500

    [badouralix] day-04

---
+++ day-04/part-1/badouralix.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+func run(s string) int {
+	var sleepmaxminute int
+	var sleepmaxguard int
+	var sleepstart int
+	var sleepend int
+	var sleepmax int
+	var guard int
+
+	sleep := make(map[int][]int)
+	sleeptotal := make(map[int]int)
+
+	input := sort.StringSlice(strings.Split(s, "\n"))
+	sort.Sort(input)
+
+	for _, line := range input {
+		switch {
+		case strings.Contains(line, "sleep"):
+			sleepstart, _ = strconv.Atoi(strings.Split(strings.Split(line, ":")[1], "]")[0])
+
+		case strings.Contains(line, "wake"):
+			sleepend, _ = strconv.Atoi(strings.Split(strings.Split(line, ":")[1], "]")[0])
+			sleeptotal[guard] += (sleepend - sleepstart)
+
+			if sleeptotal[guard] > sleepmax {
+				sleepmax = sleeptotal[guard]
+				sleepmaxguard = guard
+			}
+
+			for m := sleepstart; m < sleepend; m++ {
+				sleep[guard][m]++
+			}
+
+		default:
+			guard, _ = strconv.Atoi(strings.Split(strings.Split(line, "#")[1], " ")[0])
+			if _, ok := sleep[guard]; !ok {
+				sleep[guard] = make([]int, 60)
+			}
+			if _, ok := sleeptotal[guard]; !ok {
+				sleeptotal[guard] = 0
+			}
+		}
+	}
+
+	for m := 0; m < 60; m++ {
+		if sleep[sleepmaxguard][m] > sleep[sleepmaxguard][sleepmaxminute] {
+			sleepmaxminute = m
+		}
+	}
+
+	return sleepmaxguard * sleepmaxminute
+}
+
+func main() {
+	input, err := ioutil.ReadAll(os.Stdin)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(run(string(input)))
+}
