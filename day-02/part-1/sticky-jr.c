0b9f8a3c9941443829e0b02cb238a01c2d981735
R.G <bobby@tables.com>
2018-12-04 09:56:35 +0100

    Meow?

---
+++ day-02/part-1/sticky-jr.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+unsigned int n_dbl = 0;
+unsigned int n_tpl = 0;
+unsigned int c_dbl = 0;
+unsigned int c_tpl = 0;
+unsigned int ctr = 0;
+char p = 0;
+
+int main(int argc, char** argv) {
+	if (argc != 2) return 1;
+	char* input = argv[1];
+	while (*input != 0) {
+		if (*input == '\n') {
+			if (c_dbl > 0) {++n_dbl; c_dbl = 0;}
+			if (c_tpl > 0) {++n_tpl; c_tpl = 0;}
+			p   = 0;
+			ctr = 0;
+		}
+		if (*input == p) {
+			switch (++ctr) {
+				case 1 : ++c_dbl; break;
+				case 2 : --c_dbl; ++c_tpl; break;
+				case 3 : --c_tpl; break;
+				default: break;
+			}
+		}
+		else { ctr = 0; }
+		p = *input;
+		++input;
+	}
+	if (c_dbl > 0) ++n_dbl; 
+	if (c_tpl > 0) ++n_tpl;
+
+	printf("%d\n", n_dbl * n_tpl);
+	return 0;
+}
