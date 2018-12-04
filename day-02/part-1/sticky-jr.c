f3b7219faf5527750b524449aeea49e29b3c2664
R.G <bobby@tables.com>
2018-12-04 10:26:59 +0100

    Maybe this was the bug?

--- day-02/part-1/sticky-jr.c
+++ day-02/part-1/sticky-jr.c
@@ -9,11 +9,12 @@ char p = 0;
 
 int main(int argc, char** argv) {
 	if (argc != 2) return 1;
+	
 	char* input = argv[1];
 	while (*input != 0) {
 		if (*input == '\n') {
-			if (c_dbl > 0) {++n_dbl; c_dbl = 0;}
-			if (c_tpl > 0) {++n_tpl; c_tpl = 0;}
+			if (c_dbl > 0 || ctr == 1) {++n_dbl; c_dbl = 0;}
+			if (c_tpl > 0 || ctr == 2) {++n_tpl; c_tpl = 0;}
 			p   = 0;
 			ctr = 0;
 		}
@@ -29,8 +30,8 @@ int main(int argc, char** argv) {
 		p = *input;
 		++input;
 	}
-	if (c_dbl > 0) ++n_dbl; 
-	if (c_tpl > 0) ++n_tpl;
+	if (c_dbl > 0 || ctr == 1) ++n_dbl; 
+	if (c_tpl > 0 || ctr == 2) ++n_tpl;
 
 	printf("%d\n", n_dbl * n_tpl);
 	return 0;
