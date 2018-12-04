9ef3b540f66f94731a517d5a266b884f793afca0
Aimery Tauveron <atauveron@via.ecp.fr>
2018-12-04 11:33:27 +0100

    [atauveron] input for day-04
    input for day-04
    optim attempt for day-03

--- day-03/part-2/atauveron.cpp
+++ day-03/part-2/atauveron.cpp
@@ -1,13 +1,12 @@
 #include <array>
-#include <exception>
 #include <iostream>
 #include <sstream>
 #include <string>
-#include <set>
+#include <unordered_set>
 #include <vector>
 
-inline std::vector<std::array<int, 5>> read_input(std::string input_str) {
-  std::vector<std::array<int, 5>> result{};
+inline std::vector<std::array<int, 4>> read_input(std::string input_str) {
+  std::vector<std::array<int, 4>> result{};
   std::stringstream input(input_str, std::ios_base::in);
   std::string line;
   while (std::getline(input, line)) {
@@ -16,34 +15,35 @@ inline std::vector<std::array<int, 5>> read_input(std::string input_str) {
     unsigned int index3 = line.find(':');
     unsigned int index4 = line.find('x');
     // Values
-    int id = std::stoi(line.substr(1, index1 - 2));
     int left = std::stoi(line.substr(index1 + 1, index2 - index1 - 1));
     int top = std::stoi(line.substr(index2 + 1, index3 - index2 - 1));
     int width = std::stoi(line.substr(index3 + 1, index4 - index3 - 1));
     int height = std::stoi(
         line.substr(index4 + 1, line.size() - index4)); // +1 -1 cancels out
-    result.push_back({left, top, width, height, id});
+    result.push_back({left, top, width, height});
   }
   return result;
 }
 
 int run(std::string input_str) {
-  std::vector<std::array<int, 5>> input(read_input(input_str));
+  std::vector<std::array<int, 4>> input(read_input(input_str));
   std::array<std::array<int, 1000>, 1000> fabric{};
-  std::set<int> non_overlap{};
-  for (auto &it : input){
-    bool overlap (false);
+  std::unordered_set<int> non_overlap{};
+  int index (0);
+  for (auto &it : input) {
+    ++index;
+    bool overlap(false);
     for (int i = it[0]; i < it[0] + it[2]; ++i) {
       for (int j = it[1]; j < it[1] + it[3]; ++j) {
-        if (fabric[i][j] != 0){
+        if (fabric[i][j] != 0) {
           non_overlap.erase(fabric[i][j]);
           overlap = true;
         }
-        fabric[i][j] = it[4];
+        fabric[i][j] = index;
       }
     }
-    if (!overlap){
-      non_overlap.insert(it[4]);
+    if (!overlap) {
+      non_overlap.insert(index);
     }
   }
   return *(non_overlap.begin());
