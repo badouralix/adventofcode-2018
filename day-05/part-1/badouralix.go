d5a1946493b30500883a729565719bd13eaf8aa4
Ayaz Badouraly <ayaz.badouraly@via.ecp.fr>
2018-12-05 02:23:49 -0500

    [badouralix] change data structures

--- day-05/part-1/badouralix.go
+++ day-05/part-1/badouralix.go
@@ -1,31 +1,70 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"os"
 	"strings"
 )
 
-func run(s string) int {
-	i := 0
-
-	for {
-		if strings.ToLower(string(s[i])) == strings.ToLower(string(s[i+1])) && s[i] != s[i+1] {
-			s = s[:i] + s[i+2:]
-			if i = i - 1; i < 0 {
-				i = 0
-			}
-		} else {
-			i++
-		}
-
-		if i == len(s)-1 {
-			break
-		}
+type (
+	node struct {
+		value byte
+		next  *node
 	}
 
-	return len(s)
+	stack struct {
+		top  *node
+		size int
+	}
+)
+
+func (this stack) Len() int {
+	return this.size
+}
+
+func (this stack) Peek() (byte, error) {
+	if this.size == 0 {
+		return '\x00', errors.New("Cannot peek, stack is empty")
+	}
+
+	return this.top.value, nil
+}
+
+func (this *stack) Pop() (stack, error) {
+	if this.size == 0 {
+		return stack{}, errors.New("Cannot pop, stack is empty")
+	}
+
+	return stack{this.top.next, this.size - 1}, nil
+}
+
+func (this *stack) Push(c byte) stack {
+	n := node{c, this.top}
+	return stack{&n, this.size + 1}
+}
+
+func run(s string) int {
+	st := stack{nil, 0}
+
+	for i := 0; i < len(s); i++ {
+		c, err := st.Peek()
+
+		if err != nil {
+			st = st.Push(s[i])
+			continue
+		}
+
+		if strings.ToLower(string(s[i])) != strings.ToLower(string(c)) || s[i] == c {
+			st = st.Push(s[i])
+			continue
+		}
+
+		st, _ = st.Pop()
+	}
+
+	return st.Len()
 }
 
 func main() {
