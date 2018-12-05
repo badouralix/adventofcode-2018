380000bcaad7d11edf5843e855a8551d48713c74
Silvestre Perret <silvestre.perret@gmail.com>
2018-12-05 23:33:37 +0100

    silvestre/day-5 opti

--- day-05/part-1/silvestre.py
+++ day-05/part-1/silvestre.py
@@ -5,11 +5,10 @@ from runners.python import SubmissionPy
 class SilvestreSubmission(SubmissionPy):
 
     def run(self, s):
-        s = s.splitlines()[0]
         ret = deque()
         for el1 in s:
             el2 = ret[-1] if ret else ""
-            if ret and el1.islower() != el2.islower() and el1.lower() == el2.lower():
+            if el1 != el2 and el1.lower() == el2.lower():
                 ret.pop()
             else:
                 ret.append(el1)
