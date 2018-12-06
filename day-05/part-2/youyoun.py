45fa0f5d043da62b0b8da7a1296b00d9b37a1ab5
Younes Belkouchi <belkouchiyounes@gmail.com>
2018-12-06 23:06:58 +0100

    youyoun/day-5

---
+++ day-05/part-2/youyoun.py
@@ -0,0 +1,17 @@
+from runners.python import SubmissionPy
+
+
+class YouyounSubmission(SubmissionPy):
+
+    def run(self, s):
+        polymer_lengths = {}
+        for i in range(65, 91):
+            polymer = s.replace(chr(i), "").replace(chr(i+32), "")
+            str_ = []
+            for c in polymer:
+                if str_ and abs(ord(c) - ord(str_[-1])) == 32:
+                    str_.pop()
+                else:
+                    str_.append(c)
+            polymer_lengths[i] = len(str_)
+        return min(polymer_lengths.values())
