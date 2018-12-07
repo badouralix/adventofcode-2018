d441cb72c9870fcec57f1e80a6640f530d72f40f
R.G <bobby@tables.com>
2018-12-07 14:48:27 +0100

    Less troll more fast

--- day-01/part-2/sticky-jr.py
+++ day-01/part-2/sticky-jr.py
@@ -3,13 +3,12 @@ from runners.python import SubmissionPy
 class StickySubmission(SubmissionPy):
 
     def run(self, s):
-        l = s.split('\n')
-        L = len(l)
+        l = s.splitlines()
         d = [0]
-        s = i = 0
+        s = 0
         while True:
-            s += int(l[i%L])
-            if s in d:
-                return s
-            d.append(s)
-            i += 1
+            for f in l:
+              s += int(f)
+              if s in d:
+                  return s
+              d.append(s)
