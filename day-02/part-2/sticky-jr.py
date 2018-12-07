e1e07bbfb14ba52384a0a04c3545c9c8814211b9
R.G <bobby@tables.com>
2018-12-07 16:22:13 +0100

    day 2 part 2

---
+++ day-02/part-2/sticky-jr.py
@@ -0,0 +1,16 @@
+from runners.python import SubmissionPy
+
+
+class StickySubmission(SubmissionPy):
+
+    def run(self, s):
+        lines = s.splitlines()
+        lines.sort()
+        rev = sorted(lines, key=lambda x: x[::-1])
+        
+        for lst in (lines, rev):
+            for i, row1 in enumerate(lst[:-1]):
+                row2 = lst[i+1]
+                out = ''.join([x for x, y in zip(row1, row2) if x==y])
+                if len(out) == len(row1) - 1:
+                    return out
