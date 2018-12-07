8148f2314c4b5ab4a345dc29480cf0e3a9846602
R.G <bobby@tables.com>
2018-12-07 15:01:39 +0100

    Fixy fixy?

--- day-01/part-2/sticky-jr.py
+++ day-01/part-2/sticky-jr.py
@@ -3,12 +3,12 @@ from runners.python import SubmissionPy
 class StickySubmission(SubmissionPy):
 
     def run(self, s):
-        l = s.splitlines()
+        l = [int(f) for f in s.splitlines()]
         d = [0]
         s = 0
         while True:
             for f in l:
-              s += int(f)
+              s += f
               if s in d:
                   return s
               d.append(s)
