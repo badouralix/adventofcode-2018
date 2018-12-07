6b017179029f70272858a967cded87ab3eb7164c
Aimery Tauveron <atauveron@via.ecp.fr>
2018-12-07 18:16:37 +0100

    [atauveron] day-06 part-1 optim attempt

--- day-06/part-1/atauveron.cpp
+++ day-06/part-1/atauveron.cpp
@@ -5,18 +5,17 @@
 #include <utility>
 #include <vector>
 
-inline int_fast16_t abs_diff(unsigned int a, unsigned int b) {
+inline int_fast16_t abs_diff(uint_fast16_t a, uint_fast16_t b) {
   return (int_fast16_t)((a > b) ? a - b : b - a);
 }
 
-inline std::vector<std::pair<unsigned int, unsigned int>>
+inline std::vector<std::pair<uint_fast16_t, uint_fast16_t>>
 parse_input(std::string &input_str) {
   std::stringstream input(input_str, std::ios_base::in);
-  std::vector<std::pair<unsigned int, unsigned int>> result{};
+  std::vector<std::pair<uint_fast16_t, uint_fast16_t>> result{};
   std::string line;
   while (std::getline(input, line)) {
-    unsigned int x;
-    unsigned int y;
+    unsigned int x, y;
     sscanf(line.c_str(), "%u %*c %u", &x, &y);
     result.push_back({x, y});
   }
@@ -25,10 +24,10 @@ parse_input(std::string &input_str) {
 
 int run(std::string input_str) {
   // Parse the input
-  const std::vector<std::pair<unsigned int, unsigned int>> input(
+  const std::vector<std::pair<uint_fast16_t, uint_fast16_t>> input(
       parse_input(input_str));
   // Extrema
-  unsigned int x_max(input.at(0).first), x_min(input.at(0).first),
+  uint_fast16_t x_max(input.at(0).first), x_min(input.at(0).first),
       y_max(input.at(0).second), y_min(input.at(0).second);
   for (auto &it : input) {
     if (it.first > x_max) {
@@ -44,24 +43,25 @@ int run(std::string input_str) {
       y_min = it.second;
     }
   }
-  // Array
-  const unsigned int size_x(x_max), size_y(y_max);
+  // Array containing the closest node to any point
+  const uint_fast16_t size_x(x_max), size_y(y_max);
+  const uint_fast16_t nb_points(input.size());
   int_fast16_t closest[size_x][size_y];
-  for (unsigned int i = 0; i < size_x; ++i) {
-    for (unsigned int j = 0; j < size_y; ++j) {
-      std::vector<int_fast16_t> dist(input.size(), 5000);
-      for (unsigned int id = 0; id < dist.size(); ++id) {
+  for (uint_fast16_t i = 0; i < size_x; ++i) {
+    for (uint_fast16_t j = 0; j < size_y; ++j) {
+      std::vector<int_fast16_t> dist(nb_points, 5000);
+      for (uint_fast8_t id = 0; id < nb_points; ++id) {
         dist.at(id) =
-            abs_diff(i, input.at(id).first) + abs_diff(j, input.at(id).second);
+            abs_diff(i, input[id].first) + abs_diff(j, input[id].second);
       }
       // Find the closest node
       int_fast16_t node(0);
       int counter(1);
-      for (unsigned int id = 1; id < dist.size(); ++id) {
-        if (dist.at(id) < dist.at(node)) {
+      for (uint_fast8_t id = 1; id < nb_points; ++id) {
+        if (dist[id] < dist[node]) {
           node = id;
           counter = 1;
-        } else if (dist.at(id) == dist.at(node)) {
+        } else if (dist[id] == dist[node]) {
           counter += 1;
         }
       }
@@ -69,55 +69,47 @@ int run(std::string input_str) {
         closest[i][j] = node;
       } else if (counter > 1) {
         closest[i][j] = -1;
-      } else {
-        std::cerr << "Error for " << i << '-' << j << '\n';
       }
     }
   }
-  std::vector<int_fast16_t> area(input.size(), 0);
-  for (unsigned int i = 0; i < size_x; ++i) {
-    for (unsigned int j = 0; j < size_y; ++j) {
+  std::vector<int_fast16_t> area(nb_points, 0);
+  for (uint_fast16_t i = 0; i < size_x; ++i) {
+    for (uint_fast16_t j = 0; j < size_y; ++j) {
       if (closest[i][j] != -1) {
-        area.at(closest[i][j]) += 1;
+        area[closest[i][j]] += 1;
       }
     }
   }
   // If any area involves a border cell, it is infinite
-  for (unsigned int i = 0; i < size_x; ++i) {
+  for (uint_fast16_t i = 0; i < size_x; ++i) {
     int_fast16_t node1(closest[i][0]);
     int_fast16_t node2(closest[i][size_y - 1]);
     if (node1 >= 0) {
-      area.at(node1) = -1;
+      area[node1] = -1;
     }
     if (node2 >= 0) {
-      area.at(node2) = -1;
+      area[node2] = -1;
     }
   }
-  for (unsigned int i = 0; i < size_y; ++i) {
+  for (uint_fast16_t i = 0; i < size_y; ++i) {
     int_fast16_t node1(closest[0][i]);
     int_fast16_t node2(closest[size_x - 1][i]);
     if (node1 >= 0) {
-      area.at(node1) = -1;
+      area[node1] = -1;
     }
     if (node2 >= 0) {
-      area.at(node2) = -1;
+      area[node2] = -1;
     }
   }
   // Find the maximum
   uint_fast16_t max_id(0);
-  for (unsigned int id = 0; id < area.size(); ++id) {
-    // if (input.at(id).first == x_max || input.at(id).first == x_min ||
-    //     input.at(id).second == y_max || input.at(id).second == y_min) {
-    //   // Exclude points with infinite domains
-    //   continue;
-    // } else {
-    // }
+  for (uint_fast8_t id = 0; id < nb_points; ++id) {
     // Compute the area
-    if (area.at(id) > area.at(max_id)) {
+    if (area[id] > area[max_id]) {
       max_id = id;
     }
   }
-  return area.at(max_id);
+  return area[max_id];
 }
 
 int main(int argc, char **argv) {
