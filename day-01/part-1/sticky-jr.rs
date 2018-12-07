46f1f41a616e1c57e3da8ed53ecbe9668a117a70
R.G <bobby@tables.com>
2018-12-07 13:17:34 +0100

    Fixy fixy

--- day-01/part-1/sticky-jr.rs
+++ day-01/part-1/sticky-jr.rs
@@ -3,11 +3,12 @@ use std::env::args;
 fn main() {
 
     println!(
+        "{}", 
         args().nth(1)                       // Take first command-line argument
               .expect("No input")           // Panic if absent
               .lines()                      // Decompose into lines
-              .map( |x| x.parse::<32>() )   // Convert lines into (signed) integer
-              .sum()                        // Sum all lines
+              .map(|x| x.parse::<i64>().expect("Error") ) // Convert lines into (signed) integer
+              .sum::<i64>()                        // Sum all lines
     )
 }
 
--- day-02/part-1/sticky-jr.c
+++ day-02/part-1/sticky-jr.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
+#include <string.h>
 
 unsigned int n_dbl = 0;
 unsigned int n_tpl = 0;
 unsigned int c_dbl = 0;
 unsigned int c_tpl = 0;
-unsigned int ctr = 0;
 char p = 0;
+
+static unsigned int ctr[26] =
+  { 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0
+  };
 
 int main(int argc, char** argv) {
 	if (argc != 2) return 1;
@@ -13,25 +20,21 @@ int main(int argc, char** argv) {
 	char* input = argv[1];
 	while (*input != 0) {
 		if (*input == '\n') {
-			if (c_dbl > 0 || ctr == 1) {++n_dbl; c_dbl = 0;}
-			if (c_tpl > 0 || ctr == 2) {++n_tpl; c_tpl = 0;}
-			p   = 0;
-			ctr = 0;
+			if (c_dbl > 0) {++n_dbl; c_dbl = 0;}
+			if (c_tpl > 0) {++n_tpl; c_tpl = 0;}
+			memset(ctr, 0x0, 26 * sizeof( unsigned int ));
 		}
-		if (*input == p) {
-			switch (++ctr) {
-				case 1 : ++c_dbl; break;
-				case 2 : --c_dbl; ++c_tpl; break;
-				case 3 : --c_tpl; break;
-				default: break;
+		else {
+			switch (++ctr[*input - 'a']) {
+				case 2: ++c_dbl; break;
+				case 3: ++c_tpl; --c_dbl; break;
 			}
 		}
-		else { ctr = 0; }
 		p = *input;
 		++input;
 	}
-	if (c_dbl > 0 || ctr == 1) ++n_dbl; 
-	if (c_tpl > 0 || ctr == 2) ++n_tpl;
+	if (c_dbl > 0) ++n_dbl; 
+	if (c_tpl > 0) ++n_tpl;
 
 	printf("%d\n", n_dbl * n_tpl);
 	return 0;
