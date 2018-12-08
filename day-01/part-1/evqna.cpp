ec7ff3ad86bb2f9094f7c9972de4572739c7ba00
Antoine Salon <antoine.salon@hotmail.com>
2018-12-08 09:59:03 -0800

    Day 1 update

--- day-01/part-1/evqna.cpp
+++ day-01/part-1/evqna.cpp
@@ -1,17 +1,31 @@
+#include <ctime>
 #include <iostream>
 #include <sstream>
 #include <string>
 
-int main(int argc, char *argv[]) {
-    std::string in = argv[1];
-    std::istringstream istream{in};
-    std::string line;
+using namespace std;
+
+int run(const string in) {
+    istringstream istream(in);
+    string line;
 
     int sum = 0;
-    while (std::getline(istream, line)) {
-        sum += std::stoi(line);
-    }
-    std::cout << sum << "\n";
+    while (getline(istream, line))
+        sum += stoi(line);
 
+    return sum;
+}
+
+int main(int argc, char **argv) {
+    if (argc < 2) {
+        cout << "Missing one argument" << endl;
+        exit(1);
+    }
+
+    clock_t start = clock();
+    auto answer = run(string(argv[1]));
+    
+    cout << "_duration:" << float( clock () - start ) * 1000.0 /  CLOCKS_PER_SEC << "\n";
+    cout << answer << "\n";
     return 0;
 }
