dc823b820e37a042067cacab6661e9283f2e4bf3
Antoine Salon <antoine.salon@hotmail.com>
2018-12-08 10:19:59 -0800

    Day 5 update

--- day-05/part-2/evqna.cpp
+++ day-05/part-2/evqna.cpp
@@ -1,16 +1,19 @@
 #include <algorithm>
+#include <iostream>
 #include <iterator>
 #include <cstdio>
 #include <stack>
 #include <string>
 #include <vector>
 
+using namespace std;
+
 int abs(int x) {
     return x >= 0 ? x : -x;
 }
 
-int react_polymer(const std::string& polymer) {
-    std::stack<char> stack;
+int react_polymer(const string& polymer) {
+    stack<char> stack;
 
     for (char c : polymer) {
         if (!stack.empty() && abs(c - stack.top()) == 32)   // Hack for lower/upper case check
@@ -22,28 +25,34 @@ int react_polymer(const std::string& polymer) {
     return stack.size();
 }
 
-int transform_react(std::string polymer, char atom) {
-    polymer.erase(std::remove_if(polymer.begin(), polymer.end(),
+int transform_react(string polymer, char atom) {
+    polymer.erase(remove_if(polymer.begin(), polymer.end(),
                   [=](char c) { return c == atom || c == atom - 32; }),
                   polymer.end()
     );
     return react_polymer(polymer);
 }
 
-int solve(const std::string& input) {
-    std::string alphabet = "abcdefghijklmnopqrstuvwxyz";
+int run(const string& input) {
+    string alphabet = "abcdefghijklmnopqrstuvwxyz";
 
-    std::vector<int> collapsed_lengths;
-    std::transform(alphabet.begin(), alphabet.end(), std::back_inserter(collapsed_lengths),
+    vector<int> collapsed_lengths;
+    transform(alphabet.begin(), alphabet.end(), back_inserter(collapsed_lengths),
                    [&](char atom) { return transform_react(input, atom); });
 
-    return *std::min_element(collapsed_lengths.begin(), collapsed_lengths.end());
+    return *min_element(collapsed_lengths.begin(), collapsed_lengths.end());
 }
 
-int main(int argc, char *argv[]) {
-    auto input = std::string(argv[1]);
-    
-    printf("%d\n", solve(input));
+int main(int argc, char **argv) {
+    if (argc < 2) {
+        cout << "Missing one argument" << endl;
+        exit(1);
+    }
 
+    clock_t start = clock();
+    auto answer = run(string(argv[1]));
+    
+    cout << "_duration:" << float( clock () - start ) * 1000.0 /  CLOCKS_PER_SEC << "\n";
+    cout << answer << "\n";
     return 0;
 }
