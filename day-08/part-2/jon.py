b0a1e359b525029910b7c8c2f62b1e1794222768
Jonathan Remy <jonremy@users.noreply.github.com>
2018-12-08 00:21:43 -0500

    [jon] day8

---
+++ day-08/part-2/jon.py
@@ -0,0 +1,28 @@
+from runners.python import SubmissionPy
+import collections
+
+
+class JonSubmission(SubmissionPy):
+
+    def run(self, s):
+        _, t = parse([int(x) for x in s.split(" ")])
+        return t
+
+def parse(l):
+    nbSub = l[0]
+    nbMeta = l[1]
+
+    rest = l[2:]
+    values = []
+
+    for _ in range(nbSub):
+        rest, v = parse(rest)
+        values.append(v)
+
+    metas = rest[:nbMeta]
+    rest = rest[nbMeta:]
+
+    if nbSub == 0:
+        return rest, sum(metas)
+    else:
+        return rest, sum(values[m-1] if m <= len(values) else 0 for m in metas)
