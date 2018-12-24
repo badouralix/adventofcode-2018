992a71a498285db483378bb57c4c2175539299b8
aymeric <aymeric@feedly.com>
2018-12-24 13:49:18 +0100

    Bebert day 19 part 1: cython much closer to c

--- day-19/part-1/bebert.pyx
+++ day-19/part-1/bebert.pyx
@@ -1,112 +1,119 @@
-cdef int addr(reg, a, b):
-    return reg[a] + reg[b]
+cdef void addr(int *reg, int a, int b, int c):
+    reg[c] = reg[a] + reg[b]
 
-cdef int addi(reg, a, b):
-    return reg[a] + b
+cdef void addi(int *reg, int a, int b, int c):
+    reg[c] = reg[a] + b
 
-cdef int mulr(reg, a, b):
-    return reg[a] * reg[b]
+cdef void mulr(int *reg, int a, int b, int c):
+    reg[c] = reg[a] * reg[b]
 
-cdef int muli(reg, a, b):
-    return reg[a] * b
+cdef void muli(int *reg, int a, int b, int c):
+    reg[c] = reg[a] * b
 
-cdef int banr(reg, a, b):
-    return reg[a] & reg[b]
+cdef void banr(int *reg, int a, int b, int c):
+    reg[c] = reg[a] & reg[b]
 
-cdef int bani(reg, a, b):
-    return reg[a] & b
+cdef void bani(int *reg, int a, int b, int c):
+    reg[c] = reg[a] & b
 
-cdef int borr(reg, a, b):
-    return reg[a] | reg[b]
+cdef void borr(int *reg, int a, int b, int c):
+    reg[c] = reg[a] | reg[b]
 
-cdef int bori(reg, a, b):
-    return reg[a] | b
+cdef void bori(int *reg, int a, int b, int c):
+    reg[c] = reg[a] | b
 
-cdef int setr(reg, a, b):
-    return reg[a]
+cdef void setr(int *reg, int a, int b, int c):
+    reg[c] = reg[a]
 
-cdef int seti(reg, a, b):
-    return a
+cdef void seti(int *reg, int a, int b, int c):
+    reg[c] = a
 
-cdef int gtir(reg, a, b):
+cdef void gtir(int *reg, int a, int b, int c):
     if a > reg[b]:
-        return 1
+        reg[c] = 1
     else:
-        return 0
+        reg[c] = 0
 
-cdef int gtri(reg, a, b):
+cdef void gtri(int *reg, int a, int b, int c):
     if reg[a] > b:
-        return 1
+        reg[c] = 1
     else:
-        return 0
+        reg[c] = 0
 
-cdef int gtrr(reg, a, b):
+cdef void gtrr(int *reg, int a, int b, int c):
     if reg[a] > reg[b]:
-        return 1
+        reg[c] = 1
     else:
-        return 0
+        reg[c] = 0
 
-cdef int eqir(reg, a, b):
+cdef void eqir(int *reg, int a, int b, int c):
     if a == reg[b]:
-        return 1
+        reg[c] = 1
     else:
-        return 0
+        reg[c] = 0
 
-cdef int eqri(reg, a, b):
+cdef void eqri(int *reg, int a, int b, int c):
     if reg[a] == b:
-        return 1
+        reg[c] = 1
     else:
-        return 0
+        reg[c] = 0
 
-cdef int eqrr(reg, a, b):
+cdef void eqrr(int *reg, int a, int b, int c):
     if reg[a] == reg[b]:
-        return 1
+        reg[c] = 1
     else:
-        return 0
+        reg[c] = 0
 
 
-functions = {"addr": addr, "addi": addi, "mulr": mulr, "muli": muli,
-             "banr": banr, "bani": bani, "borr": borr, "bori": bori,
-             "setr": setr, "seti": seti, "gtir": gtir, "gtri": gtri,
-             "gtrr": gtrr, "eqir": eqir, "eqri": eqri, "eqrr": eqrr}
+functions_map = {"addr":  0, "addi":  1, "mulr":  2, "muli":  3,
+                 "banr":  4, "bani":  5, "borr":  6, "bori":  7,
+                 "setr":  8, "seti":  9, "gtir": 10, "gtri": 11,
+                 "gtrr": 12, "eqir": 13, "eqri": 14, "eqrr": 15}
+
+
+cdef (void (int *, int, int, int)) *functions[16]
+functions[:] = [addr, addi, mulr, muli, banr, bani, borr, bori, setr, seti, gtir, gtri, gtrr, eqir, eqri, eqrr]
+
+
+cdef struct Instruction:
+    unsigned char fi
+    unsigned char a
+    unsigned char b
+    unsigned char c
 
 
 cpdef int run(s):
     cdef int ipb = 0
     cdef int ip = 0
     cdef int i
-    cdef int a
-    cdef int b
-    cdef int c
+    cdef Instruction ins
 
     cdef int registers[6]
     for i in range(6):
-        registers[i] =  0
+        registers[i] = 0
 
-    instructions = []
+    cdef Instruction instructions[1000]
 
+    cdef int n_instructions = 0
     for line in s.splitlines():
         if line.startswith("#ip "):
             ipb = int(line[4:])
             continue
 
         x0, x1, x2, x3 = line.split(" ")
-        instructions.append((x0, int(x1), int(x2), int(x3)))
-
-    cdef int n = len(instructions)
+        instructions[n_instructions] = Instruction(fi=functions_map[x0], a=int(x1), b=int(x2), c=int(x3))
+        n_instructions += 1
 
     # print("ip:", ip)
     # print("ipb:", ipb)
     # print("instructions:", instructions)
     # print("n:", n)
 
-    while ip < n:
-        x0, a, b, c = instructions[ip]
+    while ip < n_instructions:
+        ins = instructions[ip]
         registers[ipb] = ip
 
-        f = functions[x0]
-
-        registers[c] = f(registers, a, b)
+        functions[ins.fi](registers, ins.a, ins.b, ins.c)
         ip = registers[ipb]
         ip += 1
 
